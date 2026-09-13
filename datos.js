81c505dbcb786dff31e209cf1d671840e8e854dc
algdesarrolloit <algdesarrolloit@gmail.com>
2026-09-12 22:40:46 -0400

    Actualizacion automatica de inventario - 12/09/2026 22:40:46

--- datos.js
+++ datos.js
@@ -793,6 +793,6 @@
                           "bolivar":  0
                       }
                   ],
-    "ultima_actualizacion":  "12/09/2026 22:30:46",
+    "ultima_actualizacion":  "12/09/2026 22:40:46",
     "total_general_cajas":  40110
 };
