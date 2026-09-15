6d3bb57ad553f2f8d557e725a41d7b22852e5c26
algdesarrolloit <algdesarrolloit@gmail.com>
2026-09-15 09:20:46 -0400

    Actualizacion automatica de inventario y lotes - 15/09/2026 09:20:46

--- datos.js
+++ datos.js
@@ -134,7 +134,7 @@
                           "campo":  "cagua",
                           "codigo":  "CAG",
                           "kg":  113565.9,
-                          "cajas":  30419
+                          "cajas":  30399
                       },
                       {
                           "almacen":  "CAPITAL",
@@ -319,7 +319,7 @@
                           "margarita":  0,
                           "capital":  0,
                           "barcelona":  0,
-                          "cagua":  656,
+                          "cagua":  636,
                           "descripcion":  "MAYONESA 24 X 175GR",
                           "barquisimeto":  0,
                           "bolivar":  0
@@ -793,7 +793,7 @@
                           "bolivar":  0
                       }
                   ],
-    "ultima_actualizacion":  "15/09/2026 09:15:46",
+    "ultima_actualizacion":  "15/09/2026 09:20:46",
     "lotes":  [
                   {
                       "codigo":  "1VEG001",
@@ -4996,5 +4996,5 @@
                       "fecha_vencimiento":  "28-07-2028"
                   }
               ],
-    "total_general_cajas":  40099
+    "total_general_cajas":  40079
 };
