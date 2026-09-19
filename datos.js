37ba523c606094d92bf95d414efe2717f8490f9e
algdesarrolloit <algdesarrolloit@gmail.com>
2026-09-19 15:50:46 -0400

    Actualizacion automatica de inventario y lotes - 19/09/2026 15:50:46

--- datos.js
+++ datos.js
@@ -134,7 +134,7 @@
                           "campo":  "cagua",
                           "codigo":  "CAG",
                           "kg":  112463.9,
-                          "cajas":  35120
+                          "cajas":  35052
                       },
                       {
                           "almacen":  "CAPITAL",
@@ -787,7 +787,7 @@
                           "margarita":  0,
                           "capital":  0,
                           "barcelona":  0,
-                          "cagua":  1172,
+                          "cagua":  1138,
                           "descripcion":  "PAN BLANCO 10 X 390G",
                           "barquisimeto":  0,
                           "bolivar":  0
@@ -800,7 +800,7 @@
                           "margarita":  0,
                           "capital":  0,
                           "barcelona":  0,
-                          "cagua":  870,
+                          "cagua":  836,
                           "descripcion":  "PAN INTEGRAL 10 X 390G",
                           "barquisimeto":  0,
                           "bolivar":  0
@@ -845,7 +845,7 @@
                           "bolivar":  0
                       }
                   ],
-    "ultima_actualizacion":  "19/09/2026 15:40:58",
+    "ultima_actualizacion":  "19/09/2026 15:50:46",
     "lotes":  [
                   {
                       "codigo":  "1VEG001",
@@ -5278,5 +5278,5 @@
                       "fecha_vencimiento":  "16-10-2026"
                   }
               ],
-    "total_general_cajas":  46117
+    "total_general_cajas":  46049
 };
