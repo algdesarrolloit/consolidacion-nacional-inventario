cbd47809a5e288e90de850e4758394e5f5f138f4
algdesarrolloit <algdesarrolloit@gmail.com>
2026-09-22 18:01:03 -0400

    Actualizacion automatica de inventario y lotes - 22/09/2026 18:01:03

--- datos.js
+++ datos.js
@@ -108,7 +108,7 @@
                           "campo":  "andes",
                           "codigo":  "AND",
                           "kg":  0,
-                          "cajas":  2833
+                          "cajas":  2804
                       },
                       {
                           "almacen":  "MARGARITA",
@@ -275,7 +275,7 @@
                           "zulia":  0,
                           "codigo":  "4MMA003",
                           "um":  "CJ",
-                          "andes":  53,
+                          "andes":  24,
                           "margarita":  12,
                           "capital":  0,
                           "barcelona":  0,
@@ -792,7 +792,7 @@
                           "bolivar":  0
                       }
                   ],
-    "ultima_actualizacion":  "22/09/2026 17:50:48",
+    "ultima_actualizacion":  "22/09/2026 18:01:03",
     "lotes":  [
                   {
                       "codigo":  "1VEG001",
@@ -1696,7 +1696,7 @@
                   },
                   {
                       "codigo":  "4MMA003",
-                      "cantidad":  53,
+                      "cantidad":  24,
                       "fecha_fabricacion":  "15-09-2026",
                       "ubicacion":  "ANDES",
                       "lote":  "0327-150926-NV",
@@ -5085,5 +5085,5 @@
                       "fecha_vencimiento":  "14-10-2026"
                   }
               ],
-    "total_general_cajas":  47213
+    "total_general_cajas":  47184
 };
