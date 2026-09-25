847d261763c3a934139baff8376cf354c82bb748
algdesarrolloit <algdesarrolloit@gmail.com>
2026-09-25 01:20:47 -0400

    Actualizacion automatica de inventario y lotes - 25/09/2026 01:20:47

--- datos.js
+++ datos.js
@@ -831,7 +831,7 @@
                           "bolivar":  0
                       }
                   ],
-    "ultima_actualizacion":  "25/09/2026 01:10:47",
+    "ultima_actualizacion":  "25/09/2026 01:20:47",
     "lotes":  [
                   {
                       "codigo":  "1VEG001",
